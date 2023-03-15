375fa3080d8c865a6faea53e3cdb66163930ff7b
CSS-APP-addict <css.app.project@gmail.com>
2023-03-16 00:15:35 +0800

    Update MainContent.jsx

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function MainContent() {
     return(  
     <main>
-        <h1 className="main--title">What is all about REACTJS</h1>
+        <h1 className="main--title">This is all about REACTJS</h1>
         <ul className="main--list">
             <li>List Item 1</li>
             <li>List Item 2</li>
