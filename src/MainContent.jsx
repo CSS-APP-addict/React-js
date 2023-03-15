d1e39ba055ae944accae224808c561d2e09219b0
CSS-APP-addict <css.app.project@gmail.com>
2023-03-16 00:17:28 +0800

    Update MainContent.jsx

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -5,8 +5,8 @@ export default function MainContent() {
     <main>
         <h1 className="main--title">What is REACTJS</h1>
         <ul className="main--list">
-            <li>List Item 1</li>
-            <li>List Item 2</li>
+            <li>The React.js framework is an open-source JavaScript framework and library developed by Facebook. It’s used for building interactive user interfaces and web applications quickly and efficiently with significantly less code than you would with vanilla JavaScript.</li>
+            <li>In React, you develop your applications by creating reusable components that you can think of as independent Lego blocks. These components are individual pieces of a final interface, which, when assembled, form the application’s entire user interface. </li>
             <li>List Item 3</li>
         </ul>
     </main>
