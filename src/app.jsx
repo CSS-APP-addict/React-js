caa83f973c3b09d4289b796567512e59df7d0e07
CSS-APP-addict <css.app.project@gmail.com>
2023-03-15 20:57:30 +0800

    update

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/MainContent"
+import { useState } from 'react'
+import Navbar from "./Navbar"
+import Main from "./MainContent"
 
 export default function App() {
     return (
