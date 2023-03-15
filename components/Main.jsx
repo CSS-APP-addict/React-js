25b2190ad6bb6f5c317e0409e1e8e4ac1a6d46d9
CSS-APP-addict <css.app.project@gmail.com>
2023-03-15 20:32:03 +0800

    update

---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Main() {
+    return(  
+    <main>
+        <h1>What is all about</h1>
+        <ul>
+            <li>this is all</li>
+            <li>this is all</li>
+            <li>this is all</li>
+        </ul>
+    </main>
+) 
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+    <nav>
+        <img src="/react.svg"/>
+        <h3>Reactjs and Vite</h3>
+        <h4>More about</h4>
+    </nav>
+    )
+}
