66e5db5b25dc45533881e7dd633f46e2b52bc21b
CSS-APP-addict <css.app.project@gmail.com>
2023-03-15 23:32:17 +0800

    update

--- src/App_me.jsx
+++ src/App_me.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from "./Navbar"
 import Main from "./MainContent"
 import Footer from "./Footer"
+import './style.css'
 
 export default function App_me() {
     return (
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,9 +3,9 @@ import React from "react"
 export default function Navbar() {
     return (
     <nav>
-        <img src="/react.svg"/>
-        <h3>Reactjs and Vite</h3>
-        <h4>More about</h4>
+        <img className="nav--icon" src="/react.svg"/>
+        <h3 className="nav--logo_txt">Reactjs and Vite</h3>
+        <h4 className="nav--title">More about - Project 1</h4>
     </nav>
     )
 }
