8ebfc4fa2529a65c3d5ba23b5d474ead21bcc4e8
CSS-APP-addict <css.app.project@gmail.com>
2023-03-15 21:14:42 +0800

    d

--- src/App_me.jsx
+++ src/App_me.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Navbar from "./Navbar"
 import Main from "./MainContent"
 
-export default function App1() {
+export default function App_me() {
     return (
         <div className="container">
             <Navbar />
--- src/MainI.jsx
+++ src/MainI.jsx
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App1 from "./App1.jsx"
+import App_me from "./App_me.jsx"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App_me />, document.getElementById("root"))
