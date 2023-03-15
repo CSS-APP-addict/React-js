dc97c08d7c6d736c57690f75e8d30fdbae1fac70
CSS-APP-addict <css.app.project@gmail.com>
2023-03-15 21:34:46 +0800

    d

--- src/App_me.jsx
+++ src/App_me.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Navbar from "./Navbar"
 import Main from "./MainContent"
+import Footer from "./Footer"
 
 export default function App_me() {
     return (
         <div className="container">
             <Navbar />
             <Main />
+            <Footer />
         </div>
     )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+    return(  
+    <div>
+        <small>Copyright by Benx Dev. All right reserved.</small>
+    </div>
+) 
+}
